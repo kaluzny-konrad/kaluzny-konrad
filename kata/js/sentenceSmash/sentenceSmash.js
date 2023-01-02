6f747d57e94fb1809e4a76592af65a259e5c395d
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-01-02 08:38:37 +0100

    best solution

--- kata/js/sentenceSmash/sentenceSmash.js
+++ kata/js/sentenceSmash/sentenceSmash.js
@@ -1,15 +1,7 @@
 // https://www.codewars.com/kata/53dc23c68a0c93699800041d
 
 function smash (words) {
-    let sentence = "";
-    for (const index in words)
-        sentence += getWord(words[index], index);
-    return sentence;
+    return words.join(" ");
 };
-
-function getWord(word, index) {
-    if(index > 0) return " " + word;
-    else return word;
-}
 
 module.exports = smash;
