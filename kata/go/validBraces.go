d7090bebbf7bfba6dccc8f008633460630923329
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-01-04 18:37:30 +0100

    refactor valid braces

--- kata/go/validBraces.go
+++ kata/go/validBraces.go
@@ -1,33 +1,32 @@
 package kata
 
 func ValidBraces(str string) bool {
-
-	length := len(str)
-	for i := 0; i < length; i++ {
-		toSkip := 0
-
+	for i := 0; i < len(str); i++ {
 		openingBrace := string(str[i])
-		if isOpeningBrace(openingBrace) {
-			closingBrace := ""
+		if isClosingBrace(openingBrace) {
+			break
+		}
 
-			for j := i + 1 + toSkip; j < length; j++ {
-				brace := string(str[j])
-				if isClosingBrace(brace) {
-					closingBrace = string(str[j+toSkip])
-					break
-				} else {
-					toSkip++
-				}
-			}
-
-			validClosingBrace := getCloseBrace(openingBrace)
-			if closingBrace != validClosingBrace {
-				return false
-			}
+		closingBrace := getClosingBrace(i, str)
+		if !isValidClosingBrace(openingBrace, closingBrace) {
+			return false
 		}
 	}
 
 	return true
+}
+
+func getClosingBrace(i int, str string) string {
+	toSkip := 0
+	for j := i + 1; j < len(str); j++ {
+		brace := string(str[j])
+		if isClosingBrace(brace) {
+			return string(str[j+toSkip])
+		} else {
+			toSkip++
+		}
+	}
+	return ""
 }
 
 func isOpeningBrace(brace string) bool {
@@ -38,15 +37,15 @@ func isClosingBrace(brace string) bool {
 	return brace == ")" || brace == "]" || brace == "}"
 }
 
-func getCloseBrace(openingBrace string) string {
+func isValidClosingBrace(openingBrace string, closingBrace string) bool {
 	if openingBrace == "(" {
-		return ")"
+		return closingBrace == ")"
 	}
 	if openingBrace == "[" {
-		return "]"
+		return closingBrace == "]"
 	}
 	if openingBrace == "{" {
-		return "}"
+		return closingBrace == "}"
 	}
-	return ""
+	return false
 }
