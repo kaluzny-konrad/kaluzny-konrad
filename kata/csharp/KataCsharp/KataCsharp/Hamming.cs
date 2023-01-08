ab9e1c190de0eda36133c33a4b4751c8fb2a702b
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-01-08 10:54:52 +0100

    solution algorithm

--- kata/csharp/KataCsharp/KataCsharp/Hamming.cs
+++ kata/csharp/KataCsharp/KataCsharp/Hamming.cs
@@ -6,6 +6,18 @@ public class Hamming
 {
     public static long hamming(int n)
     {
-        return long.MaxValue;
+        var hamming = new long[n];
+        hamming[0] = 1;
+
+        long x2 = 2, x3 = 3, x5 = 5;
+        int i = 0, j = 0, k = 0;
+        for (int index = 1; index < n; index++)
+        {
+            hamming[index] = Math.Min(x2, Math.Min(x3, x5));
+            if (hamming[index] == x2) x2 = 2 * hamming[++i];
+            if (hamming[index] == x3) x3 = 3 * hamming[++j];
+            if (hamming[index] == x5) x5 = 5 * hamming[++k];
+        }
+        return hamming[n - 1];
     }
 }
