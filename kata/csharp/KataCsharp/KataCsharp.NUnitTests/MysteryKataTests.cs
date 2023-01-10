44ba980920c0084548802039b807ddb721dbd1d8
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-01-10 20:25:43 +0100

    MysteryKata done

---
+++ kata/csharp/KataCsharp/KataCsharp.NUnitTests/MysteryKataTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace KataCsharp.NUnitTests;
+
+[TestFixture]
+public class MysteryKataTests
+{
+    [Test]
+    public void Is_It_Sane()
+    {
+        var sanity = new Dictionary<string, string> { { "sanity", "hello" } };
+        Assert.AreEqual(MysteryKata.Mystery(), sanity, "Mystery has not returned to sanity.");
+    }
+}
---
+++ kata/csharp/KataCsharp/KataCsharp/MysteryKata.cs
@@ -0,0 +1,12 @@
+﻿namespace KataCsharp
+{
+    public class MysteryKata
+    {
+        public static Dictionary<string, string> Mystery()
+        {
+            Dictionary<string, string> results = new();
+            results.Add("sanity", "hello");
+            return results;
+        }
+    }
+}
